4efefec7696737bd992f82998402045152990f25
midlis@maia <midlis@gmail.com>
2025-09-22 19:19:39 +0200

    Fix flake8 problem in docker container.

--- app/user/views.py
+++ app/user/views.py
@@ -18,7 +18,7 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user."""
 
     serializer_class = AuthTokenSerializer
-    # renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
