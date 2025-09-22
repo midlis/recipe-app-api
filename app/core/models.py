fcc9ecdef8585bd396bc63622b10bde8b3f48cc2
midlis@maia <midlis@gmail.com>
2025-09-23 00:21:23 +0200

    Fix name for __str__ method.

--- app/core/models.py
+++ app/core/models.py
@@ -57,5 +57,5 @@ class Recipe(models.Model):
     description = models.TextField(blank=True)
     link = models.CharField(max_length=255, blank=True)
 
-    def __str(self):
+    def __str__(self):
         return self.title
