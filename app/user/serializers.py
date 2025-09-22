2ff13f3e200530268a11ee1040b066ded34a4bcb
midlis@maia <midlis@gmail.com>
2025-09-22 14:52:04 +0200

    Silence some pyright issues.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -2,7 +2,7 @@
 Serializers for the user API View.
 """
 
-from typing_extensions import override
+from typing_extensions import override  # pyright: ignore[reportShadowedImports]
 from django.contrib.auth import get_user_model, authenticate
 from django.utils.translation import gettext as _
 from rest_framework import serializers
@@ -23,7 +23,7 @@ class UserSerializer(serializers.ModelSerializer):
         return get_user_model().objects.create_user(**validated_data)
 
 
-class AuthTokenSerializer(serializers.Serializer):
+class AuthTokenSerializer(serializers.Serializer):  # pylint: disable=W0223
     """Serializer for the user auth token."""
 
     email = serializers.EmailField()
