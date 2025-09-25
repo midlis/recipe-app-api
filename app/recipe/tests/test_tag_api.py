2f11cfc03fb4d0a495f18347cd01cee1b9f34d75
midlis@fenix <midlis@gmail.com>
2025-09-25 15:12:25 +0200

    96. Write tests for deleting tags

--- app/recipe/tests/test_tag_api.py
+++ app/recipe/tests/test_tag_api.py
@@ -80,3 +80,14 @@ class PrivateTagsApiTests(APITestCase):
         self.assertEquals(res.status_code, status.HTTP_200_OK)
         tag.refresh_from_db()
         self.assertEquals(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """Test deleting a tag."""
+        tag = Tag.objects.create(user=self.user, name='Breakfast')
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEquals(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
